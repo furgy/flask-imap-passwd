0b27800aa49a61cd855c1b4a06c52701eb0e11a4
Shawn Furgason <git-spf@sneakemail.com>
2020-12-14 14:03:40 -0800

    Fixed typo

--- main.py
+++ main.py
@@ -74,7 +74,7 @@ if ENC_KEY is None:
 def send_email(recipient,message):
     #Create a secure SSL context
 
-    if FLASK_ENV == "development":-6+
+    if FLASK_ENV == "development":
         print(f"Recipient: {recipient}")
         print(f"Message:\n{message}")
         recipient = "shawn@furgason.com"
