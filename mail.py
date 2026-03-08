05a14e3e67d5e1c12638396136b99442ec66470a
Shawn Furgason <github-spf@sneakemail.com>
2026-03-07 20:53:01 -0500

    Consolidate workflow files to ci.yml and bump version

--- mail.py
+++ mail.py
@@ -12,6 +12,6 @@ This message is sent from Python."""
 #Create a secure SSL context
 context = ssl.create_default_context()
 
-with smtplib.SMTP_SSL("mail.furg.us", port, context=context) as server:
+with smtplib.SMTP_SSL("mail.home", port, context=context) as server:
     server.login("shawn@furg.us",password)
     server.sendmail(sender_email,receiver_email,message)
